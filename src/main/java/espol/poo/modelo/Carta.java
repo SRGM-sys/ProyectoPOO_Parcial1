d0ed408b002e76e7264993fea7db53a658dc0de2
ThaMatthias <156712078+TheMattias1127@users.noreply.github.com>
2025-11-01 20:51:49 -0500

    Carta

---
+++ src/main/java/espol/poo/modelo/Carta.java
@@ -0,0 +1,46 @@
+package espol.poo.modelo;
+
+public class Carta {
+    // Atributos
+    private String valor; // Ej: "RECIC", "ENERGIA", etc.
+    private boolean estaVolteada;
+    private boolean estaEncontrada;
+
+    // Constructor
+    public Carta(String valor) {
+        this.valor = valor;
+        this.estaVolteada = false;
+        this.estaEncontrada = false;
+    }
+
+    // Métodos (Getters y Setters)
+    public String getValor() {
+        return valor;
+    }
+
+    // Otros getters y setters como en el UML...
+    public boolean isEstaVolteada() {
+        return estaVolteada;
+    }
+
+    public void setEstaVolteada(boolean estaVolteada) {
+        this.estaVolteada = estaVolteada;
+    }
+    
+    public boolean isEstaEncontrada() {
+        return estaEncontrada;
+    }
+
+    public void setEstaEncontrada(boolean estaEncontrada) {
+        this.estaEncontrada = estaEncontrada;
+    }
+
+    // Métodos de acción
+    public void voltear() {
+        this.estaVolteada = true;
+    }
+
+    public void ocultar() {
+        this.estaVolteada = false;
+    }
+}
