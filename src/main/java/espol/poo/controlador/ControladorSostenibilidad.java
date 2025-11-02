750217470dbcd5b1030da4e2d2ecc7f5c9229e00
Erwxn-code <erwin.e.mora.baque@gmail.com>
2025-11-01 19:25:34 -0500

    Controlador y Registro  de Sostenibilidad

---
+++ src/main/java/espol/poo/controlador/ControladorSostenibilidad.java
@@ -0,0 +1,59 @@
+package espol.poo.controlador;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import espol.poo.modelo.*;
+
+public class ControladorSostenibilidad {
+    //Lista para guardar los registros
+     private List<RegistroSostenibilidad> registros;
+
+     //Constructor
+    public ControladorSostenibilidad() {
+        this.registros = new ArrayList<>();
+    }
+    //Getters y Setters
+    public List<RegistroSostenibilidad> getRegistros() {
+        return registros;
+    }
+
+    public void setRegistros(List<RegistroSostenibilidad> registros) {
+        this.registros = registros;
+    }
+
+    //ARegistrando los registros
+    public void registrarAccionesDia(RegistroSostenibilidad registro) {
+        registros.add(registro);
+    }
+    //Metodo para retonar el registro que conrresponde con el dia
+    public RegistroSostenibilidad getRegistroDia(LocalDate dia) {
+        for (RegistroSostenibilidad r : registros) {
+            if (r.getFecha().equals(dia)) {
+                return r;
+            }
+        }
+        return null;
+    }
+
+    //Metodo para resumir de los puntos de sostenibilidad obtenidos durante una semana.
+    public Map<String, Integer> getResumenSemanal(LocalDate dia) {
+        Map<String, Integer> resumen = new HashMap<>();
+        int totalPuntos = 0;
+        LocalDate inicioSemana = dia.minusDays(6);
+
+        for (RegistroSostenibilidad r : registros) {
+            if (!r.getFecha().isBefore(inicioSemana) && !r.getFecha().isAfter(dia)) {
+                totalPuntos += r.getPuntosDia();
+            }
+        }
+
+        resumen.put("TotalPuntos", totalPuntos);
+        return resumen;
+    }
+}
+
+
+
+
---
+++ src/main/java/espol/poo/modelo/RegistroSostenibilidad.java
@@ -0,0 +1,64 @@
+package espol.poo.modelo;
+import java.time.LocalDate;
+public class RegistroSostenibilidad {
+     private LocalDate fecha;
+    private boolean usoTransporteSostenible;
+    private boolean evitoImpresiones;
+    private boolean evitoEnvasesDescartables;
+    private boolean separoResiduos;
+    //Constructor
+    public RegistroSostenibilidad(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    //Getters y Setters
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public boolean isUsoTransporteSostenible() {
+        return usoTransporteSostenible;
+    }
+
+    public void setUsoTransporteSostenible(boolean usoTransporteSostenible) {
+        this.usoTransporteSostenible = usoTransporteSostenible;
+    }
+
+    public boolean isEvitoImpresiones() {
+        return evitoImpresiones;
+    }
+
+    public void setEvitoImpresiones(boolean evitoImpresiones) {
+        this.evitoImpresiones = evitoImpresiones;
+    }
+
+    public boolean isEvitoEnvasesDescartables() {
+        return evitoEnvasesDescartables;
+    }
+
+    public void setEvitoEnvasesDescartables(boolean evitoEnvasesDescartables) {
+        this.evitoEnvasesDescartables = evitoEnvasesDescartables;
+    }
+
+    public boolean isSeparoResiduos() {
+        return separoResiduos;
+    }
+
+    public void setSeparoResiduos(boolean separoResiduos) {
+        this.separoResiduos = separoResiduos;
+    }
+
+    //Metodo para sumar puntos del dia
+    public int getPuntosDia() {
+        int puntos = 0;
+        if (usoTransporteSostenible) puntos++;
+        if (evitoImpresiones) puntos++;
+        if (evitoEnvasesDescartables) puntos++;
+        if (separoResiduos) puntos++;
+        return puntos;
+    }
+}
